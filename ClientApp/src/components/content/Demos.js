39a2b0d8d0fad0d2c0f3dd48236dd000aabe7ed8
Matthew Mackay <mattmackay76@gmail.com>
2019-07-23 19:44:16 -0700

    testing out validation pattern

--- ClientApp/src/components/content/Demos.js
+++ ClientApp/src/components/content/Demos.js
@@ -6,6 +6,21 @@ import "./Demos.css";
 
 import { postTest } from '../../actions';
 
+function validate(someData, someOtherData) {
+    // true means invalid, so our conditions got reversed
+    let validation = {
+        errors: {
+            someData: someData.length === 0,
+            someOtherData: someOtherData.length === 0,
+        },
+        messages: {
+            someData: someData.length === 0 ? ['Please, a value is required'] : [],
+            someOtherData: someOtherData.length === 0 ? ['Please, a value is required', 'and also another'] : [],
+        }
+    };
+    return validation;
+}
+
 class Demos extends Component {
 
     constructor(props) {
@@ -17,6 +32,10 @@ class Demos extends Component {
             formData: {
                 someData: '',
                 someOtherData: '',
+            },
+            touched: {
+                someData: false,
+                someOtherData: false
             }
         };
     }
@@ -26,7 +45,6 @@ class Demos extends Component {
         const target = event.target;
         const value = target.type === 'checkbox' ? target.checked : target.value;
         const name = target.name;
-
 
         //NOTE: notice how ...this.state.formData is *first* before [name]:value
         //any other way the previous data overwrites what we're trying to update on this event
@@ -41,39 +59,85 @@ class Demos extends Component {
 
     handleSubmit = (event) => {
         event.preventDefault();
-        var id = this.props.test ? this.props.test.id : undefined;
+
+        if (!this.canBeSubmitted()) 
+            return;
+
+        var id = this.props.isLoaded ? this.props.test.id : undefined;
         this.props.postTest(this.state.formData, id);
     };
+
+    handleBlur = field => evt => {
+        this.setState({
+            touched: { ...this.state.touched, [field]: true }
+        });
+    };
+
+    canBeSubmitted() {
+        const validation = validate(this.state.formData.someData, this.state.formData.someOtherData);
+        const isDisabled = Object.keys(validation.errors).some(x => validation.errors[x]);
+        return !isDisabled;
+    }
 
     componentDidMount() {
     }
 
     render() {
-
         //Required logged in
         if (!this.props.isLoggedIn) {
             this.props.history.push('/'); //redirects us back to the root since user is not logged in
             return null; //do not render anything
         }
 
+        const { errors, messages } = validate(this.state.formData.someData, this.state.formData.someOtherData);
+        const isDisabled = Object.keys(errors).some(x => errors[x]);
+        const shouldMarkError = field => {
+            const hasError = errors[field];
+            const shouldShow = this.state.touched[field];
+            return hasError ? shouldShow : false;
+        };
+
+
         return (
             <article id="demos" className="content">
                 
                 <form onSubmit={this.handleSubmit}>
                     <label>
-                        <input name="someData" value={this.state.formData.someData} onChange={this.handleChange} />
-                        <input type="submit" value="Add" className="ui primary button" />
-                    </label>
-
-                    <div className="ui divider" />
-                        <div className="ui pointing above label" style2={{ display: 'none' }}>
-                            Please enter a value
+                        <span>someData: </span><input
+                            name="someData" placeholder=""
+                            value={this.state.formData.someData}
+                            className={shouldMarkError("someData") ? 'error' : ''}
+                            onChange={this.handleChange}
+                            onBlur={this.handleBlur("someData")} />
+                        <div
+                            className="ui pointing above label"
+                            style={!shouldMarkError("someData") ? { display: 'none' } : {}}>
+                            {messages["someData"].map(e => (
+                                <p>{e}</p>
+                            ))}
                         </div>
-                    <input name="someOtherData" value={this.state.formData.someOtherData} onChange={this.handleChange} />
-
+                    </label>
                     
                     <div className="ui divider" />
 
+                    <label>
+                        <span>someOtherData: </span><input
+                            name="someOtherData"
+                            className={shouldMarkError("someOtherData") ? 'error' : ''}
+                            value={this.state.formData.someOtherData}
+                            onChange={this.handleChange}
+                            onBlur={this.handleBlur("someOtherData")} />
+                        <div
+                            className="ui pointing above label"
+                            style={!shouldMarkError("someOtherData") ? { display: 'none' } : {}}>
+                            {messages["someOtherData"].map(e => (
+                                <p>{ e }</p>
+                            ))}
+                        </div>
+                    </label>
+
+                    <div className="ui divider" />
+                    <input disabled={isDisabled} type="submit" value={ !this.props.isLoaded ? 'Add': 'Update'} className="ui primary button" />
                 </form>                
             </article>
         );
